d86aa119653c43efbf86329a0471264154aa68d1
Jihoon Lim <limjilab@gmail.com>
2023-11-12 23:10:46 +0900

    43회 정기연주회 배너 작업중

--- app/page.tsx
+++ app/page.tsx
@@ -6,16 +6,17 @@ import { useEffect, useState } from 'react';
 import Slider from './components/Slider';
 
 export default function Home() {
-  
-  const slides: string[] = ['/42.jpg', '/41.jpg',];
 
   return (
     <div className={styles.container}>
-      <Slider images={slides} />
-      <h3 className={styles.text}>
-        <p>welcome to</p>
-        <p>KUCA!</p>
-      </h3>
+      <div className={styles.banner}>
+        <h3>
+          KUCA의<br/>
+          43번째 정기 연주회에<br/>
+          여러분을 초대합니다!
+          </h3>
+        <h4><pre>지휘자 유건   |   악장 이제우   |   합주 한국대학생클래식기타연합회</pre></h4>
+      </div>
     </div>
   )
 }
